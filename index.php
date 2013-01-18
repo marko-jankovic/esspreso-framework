ce0bf789c09527d2cee711199adb38ee15133d4a
Marko-Jankovic <zzzvezda@yahoo.com>
2013-01-18 18:00:58 +0100

    test

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+/* include defined paths */
+include_once 'config/defines.php';
+
+/* include defined paths */
+include_once 'config/database.php';
+
+/* include autoload (autoload function) */
+include_once LIBRARY_PATH . '/autoload.php';
+
+Autoload::getInstance();
+
+/* include routes */
+include_once 'config/routes.php';
+
+
+Espresso_Dispatcher::dispatch();
